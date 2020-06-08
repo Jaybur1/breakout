d1b6df544dbef9978d294abd08170161a99d8af3
Jaybur1 <jayburbyga@gmail.com>
2020-06-08 13:54:39 -0400

    init main

--- main.lua
+++ main.lua
@@ -108,3 +108,87 @@ function love.load()
   -- test for input from within other functions
   love.keyboard.keyPressed = {}
 end
+
+-- allow resize without breaking pixels
+function love.resize(w, h)
+  push:resize(w, h)
+end
+
+--[[
+    Called every frame, passing in `dt` since the last frame. `dt`
+    is short for `deltaTime` and is measured in seconds. Multiplying
+    this by any changes we wish to make in our game will allow our
+    game to perform consistently across all hardware; otherwise, any
+    changes we make will be applied as fast as possible and will vary
+    across system hardware.
+]]
+function love.update(dt)
+  gStateMachine:update(dt)
+
+  --reset keys pressed
+  love.keyboard.keyPressed = {}
+end
+
+--[[
+    A callback that processes key strokes as they happen, just the once.
+    Does not account for keys that are held down, which is handled by a
+    separate function (`love.keyboard.isDown`). Useful for when we want
+    things to happen right away, just once, like when we want to quit.
+]]
+function love.keypressed(key)
+  love.keyboard.keyPressed[key] = true
+end
+
+--[[
+    A custom function that will let us test for individual keystrokes outside
+    of the default `love.keypressed` callback, since we can't call that logic
+    elsewhere by default.
+]]
+function love.keyboard.wasPressed(key)
+  if love.keyboard.keyPressed[key] then
+    return true
+  else
+    return false
+  end
+end
+
+--[[
+    Called each frame after update; is responsible simply for
+    drawing all of our game objects and more to the screen.
+]]
+function love.draw()
+  push:apply("start")
+
+  --background will be drawn regardless of state
+  local backgroundWidth = gTextures["background"]:getWidth()
+  local backgroundHeight = gTextures["background"]:getHeight()
+
+  love.graphics.draw(
+    getTextures["background"],
+    0,
+     --x
+    0,
+     --y
+    0,
+     -- rotation
+    VIRTUAL_WIDTH / (backgroundWidth - 1), -- scale x
+    VIRTUAL_HEIGHT / (backgroundHeight - 1) --scale y
+  )
+  -- use the state machine to defer rendering to the current state we're in
+  gStateMachine:render()
+
+  -- display FPS for debugging; simply comment out to remove
+  displayFPS()
+
+  push:apply("end")
+end
+
+--[[
+    Renders the current FPS.
+]]
+function displayFPS()
+  -- simple FPS display across all states
+  love.graphics.setFont(gFonts["small"])
+  love.graphics.setColor(0, 255, 0, 255)
+  love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 5, 5)
+end
